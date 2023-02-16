8b80748a15803331def2f5d2fa239c36060d33fc
waaley <walebadejoko@gmail.com>
2023-02-16 11:06:55 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A programm that prints exactly with proper grammer
+ * Return: 0
+ */
+int main(void)
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
